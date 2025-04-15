608651fc0f18cf77bbe76dc52d57c52ac05c9e1e
esser <blizzz93@gmail.com>
2025-04-15 16:36:06 +0800

    fix: 404 message

--- src/server/controllers/not-found.ts
+++ src/server/controllers/not-found.ts
@@ -1,6 +1,13 @@
 import { AppError } from "../../core/errors";
 import { type Handler, respondWithError } from "../../core/server";
 
-export const notFound: Handler = app => async (__, res) => {
-  respondWithError(app, res, new AppError({ code: "NOT_FOUND" }));
+export const notFound: Handler = app => async (req, res) => {
+  respondWithError(
+    app,
+    res,
+    new AppError({
+      code: "NOT_FOUND",
+      message: `Not found: ${req.originalUrl}`,
+    }),
+  );
 };
