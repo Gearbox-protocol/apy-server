47894e3dbe738f773cdeab9bfb45a18627413279
esser <blizzz93@gmail.com>
2025-04-15 13:52:36 +0800

    fix: return back stringify

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,27 +1,9 @@
-// Wrapper around JSON stringify/parse methods to support bigint serialization
+export const json_stringify = (o: any, space?: number) => {
+  const r = JSON.stringify(
+    o,
+    (_, v) => (typeof v === "bigint" ? v.toString() : v),
+    space,
+  );
 
-function replacer(_key: string, value: any) {
-  if (typeof value === "bigint") {
-    return {
-      __type: "bigint",
-      __value: value.toString(),
-    };
-  } else {
-    return value;
-  }
-}
-
-function reviver(_key: string, value: any) {
-  if (value && value.__type === "bigint") {
-    return BigInt(value.__value);
-  }
-  return value;
-}
-
-export const json_stringify = (obj: any, space = 2) => {
-  return JSON.stringify(obj, replacer, space);
-};
-
-export const json_parse = (s: string) => {
-  return JSON.parse(s, reviver);
+  return r;
 };
