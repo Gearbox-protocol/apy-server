9edfdb5a2e2f15e72b154021cfb656671f7eab29
esser <blizzz93@gmail.com>
2025-04-15 16:36:42 +0800

    fix: add health handler

---
+++ src/server/controllers/health.ts
@@ -0,0 +1,10 @@
+import { AppError } from "../../core/errors";
+import { type Handler, respondWithError } from "../../core/server";
+
+export const checkHealth: Handler = app => async (__, res) => {
+  try {
+    res.sendStatus(200);
+  } catch (e) {
+    respondWithError(app, res, AppError.getTypedError(e));
+  }
+};
