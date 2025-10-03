4f5a56661d085deca7f5086e864a63156ff3208b
esser <blizzz93@gmail.com>
2025-10-03 09:55:15 +0700

    feat: add pt apy for plasma

--- src/tokens/apy/pendle/constants.ts
+++ src/tokens/apy/pendle/constants.ts
@@ -8,6 +8,7 @@ export const PROTOCOL = "pendle";
 export interface PendleResponse {
   underlyingApy: number;
   underlyingInterestApy: number;
+  impliedApy: number;
 }
 
 export const TOKENS: PartialRecord<
@@ -43,6 +44,37 @@ export const TOKENS: PartialRecord<
       symbol: "sUSDai",
       field: "underlyingApy",
       chainId: 42161,
+    },
+
+    "0x93b544c330f60a2aa05ced87aeeffb8d38fd8c9a": {
+      id: "0xfd3eb62302fa3cbc3c7e59e887b92dbbc814285d",
+      symbol: "PT-USDe-15JAN2026",
+      field: "impliedApy",
+      chainId: 9745,
+    },
+    "0x02fcc4989b4c9d435b7ced3fe1ba4cf77bbb5dd8": {
+      id: "0xe06c3b972ba630ccf3392cecdbe070690b4e6b55",
+      symbol: "PT-sUSDe-15JAN2026",
+      field: "impliedApy",
+      chainId: 9745,
+    },
+    "0x8dfb9a39dfab16bffe77f15544b5bf03e377e419": {
+      id: "0x18d89b23d2875590c502cd3eac8f448f3ccf9999",
+      symbol: "PT-syrupUSDT-29JAN2026",
+      field: "impliedApy",
+      chainId: 9745,
+    },
+    "0xd516188daf64efa04a8d60872f891f2cc811a561": {
+      id: "0x15735f2f53c5cd25a57dff83b11c93eceaf72073",
+      symbol: "PT-USDai-19MAR2026",
+      field: "impliedApy",
+      chainId: 9745,
+    },
+    "0xedac81b27790e0728f54dea3b7718e5437e85353": {
+      id: "0x0d7d9abee602c7f0a242ea7e200e47c372acba84",
+      symbol: "PT-sUSDai-19MAR2026",
+      field: "impliedApy",
+      chainId: 9745,
     },
   },
 };
