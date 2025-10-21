b793c752d71a08ede407d04eaf71e03b64fffed5
esser <blizzz93@gmail.com>
2025-10-21 10:50:19 +0700

    fix: add midas mre7 apy

--- src/tokens/apy/midas/constants.ts
+++ src/tokens/apy/midas/constants.ts
@@ -5,11 +5,12 @@ import type { PartialRecord } from "../../../core/utils";
 
 export const TOKENS: PartialRecord<
   NetworkType,
-  { mBASIS?: Address; mTBILL?: Address }
+  { mBASIS?: Address; mTBILL?: Address; mRe7YIELD?: Address }
 > = {
   Etherlink: {
     mTBILL: "0xdd629e5241cbc5919847783e6c96b2de4754e438",
     mBASIS: "0x2247b5a46bb79421a314ab0f0b67ffd11dd37ee4",
+    mRe7YIELD: "0x733d504435a49fc8c4e9759e756c2846c92f0160",
   },
 };
 
