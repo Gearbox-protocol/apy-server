22303665ee537d13e01270728aebe68b1c093b0a
esser <blizzz93@gmail.com>
2025-10-31 11:48:17 +0700

    feat: eth+ apy

--- src/core/app/constants.ts
+++ src/core/app/constants.ts
@@ -1,4 +1,4 @@
-export const AXIOS_CACHE_TTL = 3 * 60 * 1000;
+export const AXIOS_CACHE_TTL = 5 * 60 * 1000;
 
 export const TOKEN_APY_TASK_INTERVAL = 15 * 60 * 1000;
 export const POOL_APY_TASK_INTERVAL = 15 * 60 * 1000;
