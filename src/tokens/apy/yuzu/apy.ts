5f7d12c231d5dd6317aeb44423db2cc0988613b4
esser <blizzz93@gmail.com>
2025-11-03 19:53:07 +0800

    feat: new apys

---
+++ src/tokens/apy/yuzu/apy.ts
@@ -0,0 +1,44 @@
+import { cachedAxios } from "../../../core/app";
+import type { APYHandler, APYResult } from "../constants";
+import { PROTOCOL, TOKENS } from "./constants";
+
+interface Response {
+  yzpp_apy: number;
+  syzusd_apy: number;
+}
+
+const getUrl = () => "https://defi-api.yuzu.money/proxy/apy";
+
+const getAPYYuzu: APYHandler = async network => {
+  const tokens = TOKENS[network] || {};
+  const tokenEntries = Object.entries(tokens).map(
+    ([k, v]) => [k.toLowerCase(), v] as const,
+  );
+  if (tokenEntries.length === 0) return {};
+
+  const { data } = await cachedAxios.get<Response>(getUrl());
+
+  const syzusdApy = Number(data?.syzusd_apy || 0);
+
+  const result: APYResult = {};
+
+  if (tokens?.syzUSD) {
+    result[tokens.syzUSD] = {
+      address: tokens.syzUSD,
+      symbol: "syzUSD",
+
+      apys: [
+        {
+          address: tokens.syzUSD,
+          symbol: "syzUSD",
+          protocol: PROTOCOL,
+          value: Number(syzusdApy),
+        },
+      ],
+    };
+  }
+
+  return result;
+};
+
+export { getAPYYuzu };
---
+++ src/tokens/apy/yuzu/constants.ts
@@ -0,0 +1,12 @@
+import type { Address } from "viem";
+
+import type { NetworkType } from "../../../core/chains";
+import type { PartialRecord } from "../../../core/utils";
+
+export const TOKENS: PartialRecord<NetworkType, { syzUSD?: Address }> = {
+  Plasma: {
+    syzUSD: "0xc8a8df9b210243c55d31c73090f06787ad0a1bf6",
+  },
+};
+
+export const PROTOCOL = "yuzu";
