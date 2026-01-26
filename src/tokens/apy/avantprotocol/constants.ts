9b128818047d7930f76ec1f8383f33ddf97bcb68
de-snake <122296842+de-snake@users.noreply.github.com>
2026-01-26 20:16:57 +0300

    feat: add savUSD address on mainnet

--- src/tokens/apy/avantprotocol/constants.ts
+++ src/tokens/apy/avantprotocol/constants.ts
@@ -6,6 +6,9 @@ export const TOKENS: PartialRecord<NetworkType, { savUSD?: Address }> = {
   Plasma: {
     savUSD: "0xa29420057f3e3b9512d4786df135da1674bd74d4",
   },
+  Mainnet: {
+    savUSD: "0xb8d89678e75a973e74698c976716308abb8a46a4",
+  },
 };
 
 export const PROTOCOL = "avantprotocol";
