86119d517ab80af973a847c1c414eca32313942f
essserrr <blizzz93@gmail.com>
2026-04-27 19:18:56 +0800

    fix: remove resolv

--- src/tokens/apy/resolv/apy.ts
+++ src/tokens/apy/resolv/apy.ts
@@ -8,9 +8,10 @@ interface Response {
   value: number;
 }
 
+// WARNING: This API is down
 const getUrl = () => "https://external-api.resolv.xyz/apr/rlp?interval=7d";
-// const getStakingUrl = () =>
-//   "https://external-api.resolv.xyz/apr/staking?interval=7d";
+const getStakingUrl = () =>
+  "https://external-api.resolv.xyz/apr/staking?interval=7d";
 
 const REQUEST_CONFIG = {
   withCredentials: true,
@@ -24,16 +25,13 @@ const getAPYResolv: APYHandler = async network => {
   );
   if (tokenEntries.length === 0) return {};
 
-  const [
-    data,
-    // stakingData
-  ] = await Promise.all([
+  const [data, stakingData] = await Promise.all([
     cachedAxios.get<Response>(getUrl(), REQUEST_CONFIG),
-    // cachedAxios.get<Response>(getStakingUrl(), REQUEST_CONFIG),
+    cachedAxios.get<Response>(getStakingUrl(), REQUEST_CONFIG),
   ]);
 
   const rplRate = Number(data?.data?.value || 0);
-  // const wstUSRRate = Number(stakingData?.data?.value || 0);
+  const wstUSRRate = Number(stakingData?.data?.value || 0);
 
   const result: APYResult = {};
 
@@ -53,21 +51,21 @@ const getAPYResolv: APYHandler = async network => {
     };
   }
 
-  // if (tokens?.wstUSR) {
-  //   result[tokens.wstUSR] = {
-  //     address: tokens.wstUSR,
-  //     symbol: "wstUSR",
+  if (tokens?.wstUSR) {
+    result[tokens.wstUSR] = {
+      address: tokens.wstUSR,
+      symbol: "wstUSR",
 
-  //     apys: [
-  //       {
-  //         address: tokens.wstUSR,
-  //         symbol: "wstUSR",
-  //         protocol: PROTOCOL,
-  //         value: Number(wstUSRRate) * 100,
-  //       },
-  //     ],
-  //   };
-  // }
+      apys: [
+        {
+          address: tokens.wstUSR,
+          symbol: "wstUSR",
+          protocol: PROTOCOL,
+          value: Number(wstUSRRate) * 100,
+        },
+      ],
+    };
+  }
 
   return result;
 };
